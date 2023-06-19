6f859c595fe44d25f59a193cb0f2a101be28152a
vaishakh.thayyil <fao@faos-MacBook-Pro.local>
2023-06-19 18:40:25 +0530

    first commit

---
+++ XcuiTestBaseFramework/XcuiTestBaseFrameworkApp.swift
@@ -0,0 +1,17 @@
+//
+//  XcuiTestBaseFrameworkApp.swift
+//  XcuiTestBaseFramework
+//
+//  Created by vaishakh.thayyil on 19/06/23.
+//
+
+import SwiftUI
+
+@main
+struct XcuiTestBaseFrameworkApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
