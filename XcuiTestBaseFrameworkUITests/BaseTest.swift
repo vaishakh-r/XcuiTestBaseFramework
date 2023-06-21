d6c94e9712b3c031716bc7a2d4efd7eef0598a84
vaishakh.thayyil <fao@faos-MacBook-Pro.local>
2023-06-22 01:54:06 +0530

    Added BaseSwift package

---
+++ XcuiTestBaseFrameworkUITests/BaseTest.swift
@@ -0,0 +1,45 @@
+//
+//  BaseTest.swift
+//  XcuiTestBaseFrameworkUITests
+//
+//  Created by vaishakh.thayyil on 22/06/23.
+//
+
+import Foundation
+import XCTest
+
+
+class BaseTest: XCTestCase {
+    
+    // called before each test method
+    override func setUp() {
+        print("setUp Called")
+    }
+    
+    // called before each test class execution
+    override class func setUp() {
+        print("class func setUp Called")
+    }
+    
+    // called after each test class execution
+    override func tearDown() {
+        print("teardown Called")
+
+    }
+    
+    // called after all testcases in class executed
+    override class func tearDown() {
+        print("class func tearDown Called")
+
+    }
+    
+    func test01_call() {
+        print("Hello")
+    }
+    
+    func test02_call() {
+        print("Hello")
+    }
+    
+}
+
